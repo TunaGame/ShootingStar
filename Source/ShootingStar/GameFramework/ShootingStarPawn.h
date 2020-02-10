4e9a5958c3387dd920e47596c94f40e7511b266f
조서현 <asmisbs@gmail.com>
2020-02-10 19:51:07 +0900

    SeoHyun
    ~ enum 주석

--- Source/ShootingStar/GameFramework/ShootingStarPawn.h
+++ Source/ShootingStar/GameFramework/ShootingStarPawn.h
@@ -4,8 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "Camera/CameraComponent.h"
 #include "ShootingStarPawn.generated.h"
-
+/*
+UENUM(BlueprintType)
+enum class EStateEnum : uint8
+{
+	StateIn UMETA(DisplayName = "IN"),
+	StateOut UMETA(DisplayName = "OUT")
+};
+*/
 UCLASS()
 class SHOOTINGSTAR_API AShootingStarPawn : public APawn
 {
@@ -14,16 +26,54 @@ class SHOOTINGSTAR_API AShootingStarPawn : public APawn
 public:
 	// Sets default values for this pawn's properties
 	AShootingStarPawn();
+	/*
+	UPROPERTY(EditAnywhere, DisplayName = "CharacterState")
+	EStateEnum CurrentState;
 
+	EStateEnum PrevState;
+	*/
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	void Shooting(float NewAxisValue);
+	/*
+	DECLARE_DELEGATE(FStateDelegate)
+	FStateDelegate StateDelegate;
 
-public:	
+	UFUNCTION()
+		void OnIn();
+	UFUNCTION()
+		void OnOut();
+
+	void InStart();
+	void OutStart();
+
+	void InEnd();
+	void OutEnd();
+	*/
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(VisibleAnywhere, Category = Collision)
+		USphereComponent* Sphere;
+
+	UPROPERTY(VisibleAnywhere, Category = Visual)
+		UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere, Category = Movement)
+		UFloatingPawnMovement* Movement;
+
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+		USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+		UCameraComponent* Camera;
+	/*
+	UPROPERTY(EditAnywhere)
+		EStateEnum InOut;
+		*/
 };
