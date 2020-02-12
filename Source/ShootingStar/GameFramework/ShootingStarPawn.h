b618d242b3079c4af59b13120fdc4c79976619ed
조서현 <asmisbs@gmail.com>
2020-02-12 12:33:36 +0900

    d
    d

--- Source/ShootingStar/GameFramework/ShootingStarPawn.h
+++ Source/ShootingStar/GameFramework/ShootingStarPawn.h
@@ -35,10 +35,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 	void Shooting();
-	/*
-	void In();
-	void Out();
-	*/
+
 	void SetState(EStateEnum NewState);
 
 public:
